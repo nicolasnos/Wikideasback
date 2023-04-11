0e775f7b0b34d73f47758c51b3912a708846a02a
Jhon Franco <jhonfranco424@gmail.com>
2023-04-11 17:15:35 -0500

    Estructura inicial del proyecto

---
+++ src/test/java/com/iasoftware/wikideas/WikIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iasoftware.wikideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
